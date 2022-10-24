131e35b370579f8b0fff1ce6d9544c9c197550fa
Eliminater74 <eliminater74@gmail.com>
2022-10-24 16:31:41 -0400

    Magisk: Fix Android.mk
    Change-Id: I75b4ce9a0532348fd1b4278a4c0361bf4eff1232

--- apps/Root/Android.mk
+++ apps/Root/Android.mk
@@ -3,18 +3,21 @@
 #################Copy apks to /system/app/###############
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_USE_ROOT, true)
 include $(CLEAR_VARS)
 LOCAL_MODULE := Magisk
 LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_PATH := $$(PRODUCT_OUT)/system/priv-app
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_SYSTEM_EXT)/priv-app
 LOCAL_SRC_FILES := $(LOCAL_MODULE)$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+#LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.sidecar
 include $(BUILD_PREBUILT)
-endif
+
+#PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 
 
 
