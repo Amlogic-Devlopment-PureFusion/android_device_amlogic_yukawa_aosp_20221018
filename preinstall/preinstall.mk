a481c35eb6b1958d5079cf49907f57c002da5d4b
Eliminater74 <eliminater74@gmail.com>
2022-10-24 17:44:17 -0400

    preinstall: Changes:
    Change-Id: I0073568b1e1047fc011c360f8b9c8c8c00a7b2e7

--- preinstall/preinstall.mk
+++ preinstall/preinstall.mk
@@ -1,2 +1,4 @@
-PRODUCT_PACKAGES += Chrome
-PRODUCT_PACKAGES += Aptoide
+PRODUCT_PACKAGES += F-Droid
+PRODUCT_PACKAGES += Go_Google_Installer_v4.8.7_apkpure.com
+PRODUCT_PACKAGES += tivimate-4610
+PRODUCT_PACKAGES += Device_ID_1.3.2_apkcombo.com
